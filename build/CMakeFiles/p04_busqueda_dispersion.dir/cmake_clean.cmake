0d045e85a05842ba511c33a825a953f1ea66c663
Eric Bermúdez <91663286+alu0101517476@users.noreply.github.com>
2024-04-16 09:29:46 +0000

    Cambios clase Martes

---
+++ build/CMakeFiles/p04_busqueda_dispersion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p04_busqueda_dispersion.dir/src/practica06_main.cc.o"
+  "p04_busqueda_dispersion"
+  "p04_busqueda_dispersion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p04_busqueda_dispersion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
