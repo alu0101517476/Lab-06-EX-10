c39a3075f12fcdccc8522ea4e173bdbe79955396
Daniel Pérez <110666246+Dpr13@users.noreply.github.com>
2024-04-23 10:32:31 +0100

    Update Sala.cc

--- src/Sala.cc
+++ src/Sala.cc
@@ -1,7 +1,21 @@
 #include "../Include/Sala.h"
 
+Sala(int aforo, int identificador) : aforo_(aforo), identificador_{identificador}, disponibilidad_(true) {
+  std::forward_list<std::pair<std::string, int>> meses_dias{{"Enero", 31}, {"Febrero", 28}, {"Marzo", 31}, {"Abril", 30}, 
+  {"Mayo", 31}, {"Junio", 30}, {"Julio", 31}, {"Agosto", 31}, {"Septiembre", 30}, {"Octubre", 31}, {"Noviembre", 30}, 
+  {"Diciembre", 31}};
+  for (const auto& mes_dia : meses_dias) {
+    mes_dia_reservas_.insert(mes_dia);
+  }
+}
+
+bool Sala::reservaValida(const std::string& mes, const int dia, int hora) {
+  
+}
+
+
 bool Sala::cambiarDisponibilidad(int numero_ocupantes) {
-  if (numero_ocupantes < aforo_ && disponibilidad_ == true) {
+  if (numero_ocupantes < aforo_ && disponibilidad_ == true && numero_ocupantes > 0) {
     std::cout << "La sala se ha ocupado con " << numero_ocupantes << " personas" << std::endl;
     disponibilidad_ = false;
     return true;
