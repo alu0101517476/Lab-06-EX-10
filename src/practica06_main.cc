d80d759d0b9f95be87f36a3d07885922e5849314
Daniel Pérez <110666246+Dpr13@users.noreply.github.com>
2024-04-23 10:33:29 +0100

    Update practica06_main.cc

--- src/practica06_main.cc
+++ src/practica06_main.cc
@@ -4,23 +4,24 @@
 
 void ayuda() {
   std::cout << "Este programa implementa el funcionamiento de un software de "
-               "gestión de red de bibliotecas\n Para ejecutar este programa "
-               "debe incluir un .txt con un catálogo de libros y ejecutar "
-               "./Software 'ruta y nombre fichero libros'"
+               << "gestión de red de bibliotecas\n Para ejecutar este programa "
+               << "debe incluir un .txt con un catálogo de libros, un fichero con los usuarios "
+               << "y ejecutar " <<
+               "./Software 'ruta y nombre fichero libros' 'ruta y nombre fichero usuarios"
             << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    std::cerr << "Error, el número de parámetros es incorrecto. \nPruebe a "
-                 "ejecutar './Software --help para obtener más información"
-              << std::endl;
-
-    return 1;
-  }
   if (static_cast<std::string>(argv[1]) == "--help") {
     ayuda();
     return 0;
+  }
+  if (argc != 3) {
+    std::cerr << "Error, el número de parámetros es incorrecto. \nPruebe a "
+              <<   "ejecutar './Software --help para obtener más información"
+              << std::endl;
+
+    return 1;
   }
   Software software_red_bibliotecas{static_cast<std::string>(argv[1]), static_cast<std::string>(argv[2])};
   software_red_bibliotecas.menu();
