508242f5ef9c6ce0cd82f65c9af71d2ccd8506ab
Adrian <alu0101547619@ull.edu.es>
2024-05-03 16:55:17 +0000

    Actualizacion menu (version 2.0)

--- src/Software.cc
+++ src/Software.cc
@@ -48,16 +48,16 @@ void Software::menu() {
   std::cout << WHITE << "Cualquier duda se podrá comunicar con estos teléfonos de contacto:\n" << BLUE << " +34 677 77 77 77\n" << BLUE << " +34 622 22 22 22\n\n" << std::endl;
   std::cout << WHITE << "O bien enviar un correo a estos e-mails:\n" << BLUE << " alu0101547619@ull.edu.es\n" << BLUE << " alu0101567211@ull.edu.es\n\n" << std::endl; 
   while (true) {
-    std::cout << WHITE <<  "         Pulsa Enter para acceder al sistema:";
+    std::cout << GRAY <<  "         Pulsa Enter para acceder al sistema:" << RESET;
     if (std::getline(std::cin, press_enter) && press_enter.empty()) {
       system("clear");
       while (true) {
         std::cout << "\nPara poder acceder a los servicios de la biblioteca será necesario realizar el inicio de sesión con las credenciales correctas:\n\n";
-        std::cout << YELLOW << " --> " << WHITE << "Ingrese " << BG_GREEN << "1" << RESET << " para dirigirte a la pantalla de iniciar sesión\n\n";
+        std::cout << YELLOW << " --> " << WHITE << "Ingrese " << BG_GREEN << "1" << RESET << " para dirigirte a la pantalla de" << RED << " iniciar sesión\n\n" << RESET;
         std::cout << "Si bien no tienes una cuenta de la bilioteca, deberás crearte una nueva cuenta:\n\n";
-        std::cout << YELLOW <<" --> " << WHITE << "Ingrese " << BG_GREEN << "2" << RESET <<  " para dirigirte a la pantalla de creación de cuenta\n\n";
+        std::cout << YELLOW <<" --> " << WHITE << "Ingrese " << BG_GREEN << "2" << RESET <<  " para dirigirte a la pantalla de "<< RED << "creación de cuenta\n\n" << RESET;
         std::cout << "Una vez realizada la acción deseada podrás salir de la biblioteca:\n\n";
-        std::cout << YELLOW <<" --> " << WHITE << "Ingrese "<< BG_GREEN << "3" << RESET << " para salir de la pantalla de inicio de la biblioteca\n\n";
+        std::cout << YELLOW <<" --> " << WHITE << "Ingrese "<< BG_GREEN << "3" << RESET << " para " << RED << "salir de la pantalla de inicio de la biblioteca\n\n" << RESET;
         std::cout << BLUE << "Opción elegida: " << RESET ;
         int opcion_inicio, opcion_segunda;
         std::cin >> opcion_inicio;
@@ -65,105 +65,111 @@ void Software::menu() {
         switch (opcion_inicio) {
           case 1:
             system("clear");
-            std::cout << "Iniciando sesión..." << std::endl;
-            std::cout << "Para poder llevar a cabo el inicio de sesión, es necesario ingresar las credenciales correctamente\n\nIntroduzca el nombre de usuario: ";
+            std::cout << CYAN <<"Iniciando sesión...\n" << RESET <<std::endl;
+            std::cout << "Para poder llevar a cabo el inicio de sesión, es necesario ingresar las credenciales correctas\n" << ORANGE <<"(*)Campo obligatorio" << WHITE <<"\n\n" << YELLOW <<  " --> "<<WHITE << "Introduzca el nombre de usuario " << ORANGE <<"*" << WHITE": ";
             std::cin >> nombre_usuario;
-            std::cout << "Introduzca la contraseña del usuario: ";
+            std::cout << YELLOW <<  "\n --> "<<WHITE <<"Introduzca la contraseña del usuario " << ORANGE <<"*" << WHITE <<": ";
             std::cin >> password;
-            std::cout << "Iniciando sesión..." << std::endl;
+            std::cout << std::endl;
             usuario.setCorreo(correo);
             usuario.setContrasena(password);
             usuario.setNombreUsuario(nombre_usuario);
             if (!iniciarSesion_(nombre_fichero_base_datos_usuario_, nombre_usuario, password)) { break; }
             while (true) {
-              std::cout << "\n\nPulsa C (continuar): ";
+              std::cout << GRAY <<"\n\nPulsa C (continuar): " << RESET;
               std::cin >> press_enter_opcion1;
               if (press_enter_opcion1 == 67 || press_enter_opcion1 == 99) {
                 system("clear");
-                std::cout << "¿Qué acción deseas realizar?\n\n";
-                std::cout << " 1) Solicitar préstamo\n";
-                std::cout << " 2) Realizar devolución\n";
-                std::cout << " 3) Consultar catálogo\n";
-                std::cout << " 4) Reserva de sala\n";
-                std::cout << " 5) Cerrar sesión\n\n";
-                std::cout << "Opción: ";
+                std::cout << "Ofrecemos un servicio de préstamo de libros para realizar una reserva online y poder recoger el material precencialmente\n\n";
+                std::cout << YELLOW << " --> " << RESET<< "Ingrese " << BG_GREEN << "1" << RESET << " para dirigirse a la pantalla de "<< RED << "préstamo de material\n\n"  << RESET;
+                std::cout << "Una vez realizado el préstamo, se podrá realizar la devolución del manterial indicando el identificador del mismo\n\n";
+                std::cout << YELLOW << " --> "  << RESET <<"Ingrese " << BG_GREEN << "2" << RESET << " para dirigirse a la pantalla de " << RED << "devolución de material\n\n"<< RESET;
+                std::cout << "También dispondremos de una gran variedad de material que se podrá consultar para inspeccionar si hay en la biblioteca\n\n";
+                std::cout << YELLOW << " -->" << RESET << " Ingrese " << BG_GREEN << "3" << RESET << " para dirigirse a la pantalla de " << RED <<"consultar catalogo\n\n" << RESET;
+                std::cout << "Contamos con salas de estudio, grabaciones y ordenadores entre otros, donde habrá que reservalas con anterioridad en la web\n\n";
+                std::cout << YELLOW << " -->" << RESET << " Ingrese " << BG_GREEN << "4" << RESET << " para dirigirse a la pantalla de " << RED << "reserva de sala\n\n" << RESET;
+                std::cout << "Para salir del sistema de la biblioetca es necesario cerrar sesión\n\n";
+                std::cout << YELLOW << " -->" << RESET << " Ingrese " << BG_GREEN << "5" << RESET <<" para " << RED <<"cerrar sesión\n\n" << RESET;
+                std::cout << BLUE << "Opción elegida: " << RESET ;
                 std::cin >> opcion_segunda;
                 switch (opcion_segunda) {
                   case 1:
                     system("clear");
+                    std::cout << BLUE <<"Cátalago disponible de la biblioteca\n\n" << RESET;
                     catalogo_.Mostrar();
-                    std::cout << "Indique el identificador del libro que desea reservar (Ej: 2 (La Celestina)): ";
+                    std::cout << BLUE << "\nIndique el identificador del libro que desea reservar (Ej: 2 (La Celestina)): ";
                     std::cin >> id_libro;
                     if (prestamoLibros_(nombre_usuario, catalogo_.getLibro(id_libro))) {
-                      std::cout << "El libro está disponible para recogida presencial." << std::endl;
+                      std::cout << GREEN <<"\n✓" << RESET << " El libro está disponible para recogida presencial." << std::endl;
                     } 
                     else {
-                      std::cout << "No se puede llevar a cabo el préstamo, pruebe en otro momento. Disculpe por las molestias." << std::endl;
+                      std::cout << RED <<"\nX" << RESET << "No se puede llevar a cabo el préstamo, pruebe en otro momento. Disculpe por las molestias." << std::endl;
                     }
                     // Esto evita el bucle infinito
                     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                     continue;
                   case 2:
                     system("clear");
+                    std::cout << BLUE <<"\nCátalago disponible de la biblioteca\n\n" << RESET;
                     catalogo_.Mostrar();
-                    std::cout << "¿Cuál es el título del libro que vas a devolver? Indique el identificador ";
+                    std::cout << BLUE <<"\n¿Cuál es el título del libro que vas a devolver? Indique el identificador " << RESET;
                     std::cin >> id_libro;
                     if (devolucion_(nombre_usuario, catalogo_.getLibro(id_libro))) {
-                      std::cout << "El libro se ha devuelto satisfactoriamente." << std::endl;
+                      std::cout <<  GREEN <<"\n✓ " << RESET << "El libro se ha devuelto satisfactoriamente." << std::endl;
                     }
                     else {
-                      std::cout << "Ha habido algún problema en la devolución. Inténtelo de nuevo más tarde." << std::endl;
+                      std::cout << RED <<"\nX " << RESET << "Ha habido algún problema en la devolución. Inténtelo de nuevo más tarde." << std::endl;
                     }
                     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                     continue;
                   case 3:
                     system("clear");
-                    std::cout << "¿Cuál es el título del libro que quieres consultar? ";
+                    std::cout << BLUE <<"¿Cuál es el título del libro que quieres consultar? " << RESET;
                     std::cin >> titulo_libro;
                     if (buscarLibro_(titulo_libro)) {
-                      std::cout << "El libro está disponible en el catálogo" << std::endl;
+                      std::cout << GREEN <<"\n✓ " << RESET << "El libro está disponible en el catálogo" << std::endl;
                     } else {
-                      std::cout << "El libro no está disponible en el catálogo" << std::endl;
+                      std::cout << RED <<"\nX " << RESET << "El libro no está disponible en el catálogo" << std::endl;
                     }
                     continue;
                   case 4: {
                     int numero_ocupantes, identificador_sala, dia_sala, hora_sala;
                     std::string mes;
                     system("clear");
-                    std::cout << "¿Cuántos van a reservar la sala? ";
+                    std::cout << YELLOW << " --> " << RESET << "¿Cuántos van a reservar la sala? ";
                     std::cin >> numero_ocupantes;
-                    std::cout << "¿Qué sala quieres reservar? ";
+                    std::cout << YELLOW << " --> " << RESET << "¿Qué sala quieres reservar? ";
                     std::cin >> identificador_sala;
-                    std::cout << "¿Mes de la reserva? ";
+                    std::cout << YELLOW << " --> " << RESET << "¿Mes de la reserva? ";
                     std::cin >> mes;
-                    std::cout << "¿Día de la reserva? ";
+                    std::cout << YELLOW << " --> " << RESET << "¿Día de la reserva? ";
                     std::cin >> dia_sala; 
-                    std::cout << "¿Hora de la reserva? ";
+                    std::cout << YELLOW << " --> " << RESET << "¿Hora de la reserva? ";
                     std::cin >> hora_sala;
                     // if (hora_sala < 9 || hora_sala > 21) {
                       // std::cout << "Hora no accesible\n\nHorario de la biblioteca: 9:00-21:00" << std::endl;
                       // continue;
                     //}
                     if (reservaSala_(numero_ocupantes, mes, identificador_sala, hora_sala, dia_sala)) {
-                      std::cout << "Reserva realizada con éxito" << std::endl;
+                      std::cout << GREEN << "\nReserva realizada con éxito" << std::endl;
                     }
                     else {
-                      std::cout << "Ha habido un problema con la reserva. Inténtelo de nuevo más tarde." << std::endl;
+                      std::cout << RED <<"\nHa habido un problema con la reserva. Inténtelo de nuevo más tarde." << std::endl;
                     }
                     continue;
                   }
                   case 5:
                     system("clear");
-                    std::cout << "Cerrando sesión...\n";
+                    std::cout << MAGENTA << "Cerrando sesión...\n" << RESET;
                     while (true) {
-                      std::cout << "\n\nPulsa C (continuar): ";
+                      std::cout << GRAY << "\n\nPulsa C (continuar): " << RESET;
                       std::cin >> press_enter_opcion1;
                       if (press_enter_opcion1 == 67 ||
                           press_enter_opcion1 == 99) {
                         break;
                       } else {
                         system("clear");
-                        std::cout << "Tecla incorrecta";
+                        std::cout << RED << "\nTecla incorrecta" << RESET;
                         continue;
                       }
                     }
@@ -171,37 +177,38 @@ void Software::menu() {
                     break;
                   default:
                     system("clear");
-                    std::cout << "Error: Opción incorrecta\n\n";
-                    break;
+                    std::cout << RED << "Exit Error\nLa opción introducida no existe\n\n" << RESET;
+                    continue;
                 }
                 break;
               } else {
                 system("clear");
-                std::cout << "Tecla incorrecta";
+                std::cout << RED << "Tecla incorrecta" << RESET;
                 continue;
               }
             }
             break;
           case 2:
             system("clear");
-            std::cout << "Para llevar a cabo la creación de una cuenta de usuario será necesario introducir las credenciales deseadas por el usuario\n\nIntroduzca el nombre de usuario: ";
+            std::cout << "Para llevar a cabo la creación de una cuenta de usuario será necesario introducir las credenciales deseadas por el usuario" << ORANGE << "\n(*)Campo obligatorio" << WHITE << "\n\n" << YELLOW <<" --> " << RESET <<"Introduzca el nombre de usuario " << ORANGE << "*" << RESET <<": ";
             std::cin >> nombre_usuario;
-            std::cout << "Introduzca la contraseña del usuario: ";
+            std::cout << YELLOW <<  "\n --> " << RESET <<"Introduzca la contraseña del usuario" << ORANGE << " *" << RESET <<  ": ";
             std::cin >> password;
-            std::cout << "Introduzca el correo del usuario: ";
+            std::cout << YELLOW << "\n -->" << RESET << " Introduzca el correo del usuario" << ORANGE << " *" << RESET <<  ": ";
             std::cin >> correo;
             std::cout << "\nVerificando credenciales ingresadas";
-            system("sleep(2)");
-            std::cout << "\nCreando cuenta...";
+            sleep(1);
+            std::cout << CYAN << "\nCreando cuenta...\n" << RESET;
+            sleep(1);
             crearUsuario_(nombre_fichero_base_datos_usuario_, nombre_usuario, password, correo);
             while (true) {
-              std::cout << "\n\nPulsa C (continuar): ";
+              std::cout << GRAY << "\n\nPulsa C (continuar): " << RESET;
               std::cin >> press_enter_opcion1;
               if (press_enter_opcion1 == 67 || press_enter_opcion1 == 99) {
                 break;
               } else {
                 system("clear");
-                std::cout << "Tecla incorrecta";
+                std::cout << RED << "Tecla incorrecta";
                 continue;
               }
             }
@@ -209,7 +216,7 @@ void Software::menu() {
             break;
           case 3:
             system("clear");
-            std::cout << MAGENTA << "\n\n                       GRACIAS POR SU VISITA\n\n\n\n";
+            std::cout << MAGENTA << "\n\n               [        GRACIAS POR SU VISITA        ]        \n\n\n\n";
             exit(1);
           default:
             system("clear");
@@ -219,7 +226,7 @@ void Software::menu() {
       }
     } else {
       system("clear");
-      std::cout << "Tecla incorrecta\n\n";
+      std::cout << RED << "Tecla incorrecta\n\n" << RESET;
       continue;
     }
   }
@@ -233,19 +240,19 @@ bool Software::iniciarSesion_(const std::string& nombre_fichero_usuarios, const 
       std::string password_x;  
       fichero_usuario >> password_x;
       if (DCifra(password_x) == password) {
-        std::cout << "Inicio de sesión satisfactorio" << std::endl;
+        std::cout << GREEN <<"Inicio de sesión satisfactorio" << WHITE << std::endl;
         fichero_usuario.close();
         return true;
       }
       else {
-        std::cout << "No se ha podido iniciar sesión. Contraseña incorrecta" << std::endl;
+        std::cout << RED << "No se ha podido iniciar sesión. Contraseña incorrecta" << WHITE << std::endl;
         fichero_usuario.close();
         return false;
       }
     }
     std::getline(fichero_usuario, linea);
   }
-  std::cout << "No se ha encontrado el usuario. No se ha iniciado sesión" << std::endl;
+  std::cout << RED <<"No se ha encontrado el usuario. No se ha iniciado sesión" << WHITE <<  std::endl;
   fichero_usuario.close();
   return false;
 }
@@ -258,7 +265,7 @@ bool Software::crearUsuario_(const std::string& nombre_fichero_usuarios, const s
     std::getline(fichero_usuario, linea);
     if (nombre_usuario == nombre_usuario_x) {
       usuario_correo_existentes = true;
-      std::cout << "El correo que ha intrucido se encuentra en uso, prueba a introducir otro correo" << std::endl;
+      std::cout << RED << "\nEl correo que ha intrucido se encuentra en uso, prueba a introducir otro correo" << RESET <<std::endl;
       return false;
     }
   }
@@ -270,7 +277,7 @@ bool Software::crearUsuario_(const std::string& nombre_fichero_usuarios, const s
   std::ofstream fichero_usuario_(nombre_fichero_usuarios, std::ios::app);
   fichero_usuario_ << nombre_usuario << " " << Cifra(password) << " " << correo << std::endl;
   fichero_usuario_.close();
-  std::cout << "Cuenta creada satisfactoriamente." << std::endl;
+  std::cout << GREEN << "\nCuenta creada satisfactoriamente."  << RESET << std::endl;
   return true;
 }
 
@@ -325,7 +332,7 @@ bool Software::reservaValida_(const std::string& mes, const int dia, int hora) {
 
 bool Software::reservaSala_(int numero_ocupantes, const std::string& mes, int identificador_sala, int dia_sala, int hora_sala) {
   if (reservaValida_(mes, dia_sala, hora_sala)) {
-    std::cerr << "Error, la fecha introducida no es válida para realizar una reserva " << std::endl;
+    std::cerr << RED <<"\nError, la fecha introducida no es válida para realizar una reserva " << RESET <<std::endl;
     return false;
   }
   for (auto sala : salas_) {
